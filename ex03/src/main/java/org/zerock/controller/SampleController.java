38ad225f07434215d2e331277ec857f985f88ab5
koitt03a-PC\koitt03a <zhfxmtkachdt@daum.net>
2020-08-03 14:49:13 +0900

    2020.08.03(Mon) Spring part7 ~p.651

--- ex03/src/main/java/org/zerock/controller/SampleController.java
+++ ex03/src/main/java/org/zerock/controller/SampleController.java
@@ -1,6 +1,7 @@
 package org.zerock.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j;
@@ -12,6 +13,24 @@ public class SampleController {
 	@RequestMapping("")
 	public void basic() {
 		log.info("basic..........................");
+	}
+	
+	@GetMapping("/all")
+	public void doAll() {
+		
+		log.info("do all can access everybody");
+	}
+	
+	@GetMapping("/member")
+	public void doMember() {
+		
+		log.info("logined member");
+	}
+	
+	@GetMapping("/admin")
+	public void doAdmin() {
+		
+		log.info("admin only");
 	}
 
 }
